53110ea95b4a8c8cec47de4a38b83837a34f5ae0
Gerco van Ingen <Gercovaningen007@hotmail.com>
2024-04-25 10:11:33 +0200

    Leo made a working sidebar for himself

--- components/sidebar.php
+++ components/sidebar.php
@@ -1,13 +1,23 @@
 <div class="is-widescreen">
       <div class="columns">
-        <aside class="menu">
-          <p class="menu-label">
-            General
-          </p>
-          <ul class="menu-list">
-            <li><a>Home</a></li>
-            <li><a>Zoeken</a></li>
-            <li><a class="is-fixed-bottom">logout</a></li>
-          </ul>
+        <aside class="menu is-fullheight">
+          <b>
+            InstaClone
+          </b>
+            <div class="menu-list flex-container is-fullheight">
+              <div>
+                <a href="./">Home</a>
+                <a href="?">Zoeken</a>
+              </div>
+
+              <div style="flex:1;"></div>
+
+              <div>
+                <a id="logout" href="?">Profiel</a>
+                <a href="/login?logout=true" class="is-fixed-bottom">logout</a>
+              </div>
+            </div>
+        
+
         </aside>
         <div class="column content">
